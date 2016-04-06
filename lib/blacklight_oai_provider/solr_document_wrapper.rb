3574e91176fdfa935bc86aae68599ecc6840ba30
Eli Zoller <zoller.eli@gmail.com>
2016-04-06 13:55:50 -0400

    forget has_key

--- lib/blacklight_oai_provider/solr_document_wrapper.rb
+++ lib/blacklight_oai_provider/solr_document_wrapper.rb
@@ -31,9 +31,9 @@ module BlacklightOaiProvider
         puts @options
         puts @controller.params
         # 2016-04-06T17:09:49Z
-        if @controller.params?(:from)
+        if @controller.params.has_key?(:from)
           @controller.params[:fq] << "system_create_dtsi:[" + @controller.params?(:from) + " TO NOW]"
-          response, records = @controller.get_search_results(@controller.params, {:sort => @timestamp_field + ' asc', :rows => @limit, :fq => "system_create_dtsi:[" + @controller.params?(:from) + " TO NOW]"})
+          response, records = @controller.get_search_results(@controller.params, {:sort => @timestamp_field + ' asc', :rows => @limit, :fq => "system_create_dtsi:[" + @controller.params(:from) + " TO NOW]"})
         else
           response, records = @controller.get_search_results(@controller.params, {:sort => @timestamp_field + ' asc', :rows => @limit})
         end
